cdd33c0c916a0ec30bfd141c324449b2a2ddeaf2
Bauer Hannsen <rkaerner@gmx.net>
2015-01-17 18:16:24 +0100

    v1.2.11; modifications for working together with HUD Hider

--- RealTime.lua
+++ RealTime.lua
@@ -3,8 +3,8 @@ local metadata = {
 "## Title: RealTime",
 "## Notes: Uhrzeit in Game",
 "## Author: Marhu",
-"## Version: 1.2.10",
-"## Date: 01.11.2014",
+"## Version: 1.2.11",
+"## Date: 17.01.2015",
 "## Web: http://marhu.net",
 "## Notes: Improvement by Bauer Hannsen"
 }
@@ -36,10 +36,14 @@ function RealTime:draw()
 		if WeekDay == 0 then
 			setTextColor(1, 0, 0, 1);
 		end;
-		renderText(g_currentMission.weatherTimeBackgroundOverlay.x + g_currentMission.weatherTimeBackgroundWidth - g_currentMission.timeOffsetRight, g_currentMission.weatherTimeBackgroundOverlay.y + (self.h*3), self.h, g_i18n:getText("HUD_day") .. ": " .. g_currentMission.environment.currentDay .. " (" .. g_i18n:getText(tostring(WeekDay)) .. ")");
+		local weatherWidth = g_currentMission.weatherTimeBackgroundWidth;
+		if not g_currentMission.showWeatherForecast then
+			weatherWidth = g_currentMission.timeBackgroundWidth;
+		end;
+		renderText(g_currentMission.weatherTimeBackgroundOverlay.x + weatherWidth - g_currentMission.timeOffsetRight, g_currentMission.weatherTimeBackgroundOverlay.y + (self.h*3), self.h, g_i18n:getText("HUD_day") .. ": " .. g_currentMission.environment.currentDay .. " (" .. g_i18n:getText(tostring(WeekDay)) .. ")");
 		setTextColor(0, 0, 0, 1);
 		setTextAlignment(RenderText.ALIGN_RIGHT);
-		renderText(g_currentMission.weatherTimeBackgroundOverlay.x + g_currentMission.weatherTimeBackgroundWidth - g_currentMission.timeOffsetRight, g_currentMission.weatherTimeBackgroundOverlay.y + (self.h/4), self.h, tostring(getDate("%H:%M:%S")).." ");
+		renderText(g_currentMission.weatherTimeBackgroundOverlay.x + weatherWidth - g_currentMission.timeOffsetRight, g_currentMission.weatherTimeBackgroundOverlay.y + (self.h/4), self.h, tostring(getDate("%H:%M:%S")).." ");
 		setTextColor(1,1,1,1);
 		setTextAlignment(RenderText.ALIGN_LEFT);
 	end;
